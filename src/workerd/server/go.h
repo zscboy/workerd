ee54e175aeb30c1e3b7dabe91c8d469452d9c8ec
riguang <zscboy@gmail.com>
2024-05-21 19:32:57 +0800

    dll export

--- src/workerd/server/go.h
+++ src/workerd/server/go.h
@@ -17,7 +17,11 @@
 
 #include "gofs.h"
 
+#if _WIN32
+#define DLL_EXPORT __declspec(dllexport)
+#else
 #define DLL_EXPORT __attribute__((visibility("default")))
+#endif
 
 extern "C" {
   // golang call to init workerd runtime
